3a3a83cc00b2c2b363349829ce5bd0500ec93766
Zhixing Wang <zhixingwang71@outlook.com>
2015-12-06 18:58:43 -0800

    caroline finished signin and signup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('PetApp', ['ngSanitize', 'ui.router'])
+angular.module('PetApp', ['ngSanitize', 'ui.router', 'firebase'])
 .config(function($stateProvider){
 	$stateProvider
 		.state('home', {
@@ -23,5 +23,22 @@ angular.module('PetApp', ['ngSanitize', 'ui.router'])
 	
 }])
 .controller('SignupCtrl', ['$scope', '$http', function($scope, $http) {
-	
+	$scope.compareTo = function() {
+		console.log($scope.password);
+		console.log($scope.confirmPassword);
+		//check whether the password and confirm password are empty and whether they are the same.
+		if ($scope.password != null && $scope.confirmPassword != null && $scope.password === $scope.confirmPassword) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+	$scope.reset = function() {
+		location.reload();
+	};
+	$scope.submit = function() {
+
+
+
+	}
 }])
