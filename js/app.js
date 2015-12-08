e400e66fc5e6ac73da4e295f209b9bc3a68ccf34
Zhixing Wang <zhixingwang71@outlook.com>
2015-12-07 20:15:28 -0800

    fix profile viewing when no user has signed in

--- js/app.js
+++ js/app.js
@@ -188,7 +188,7 @@ angular.module('PetApp', ['ngSanitize', 'ui.router', 'firebase'])
 
 }])
 .controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
-	
+
 }])
 .controller('EditCtrl', ['$scope', '$http', function($scope, $http) {
 	
